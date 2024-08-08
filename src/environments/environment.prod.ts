2e555eec3997d35fa7f894ea02fc6a06be35d98f
Andi <andi@zreptil.de>
2024-08-08 22:49:58 +0200

    Ändere Umgebungsvariablen und Nachrichten
    Aktualisiere die "environment.prod.ts" Datei und setze den "settingsFilename", "DROPBOX_APP_KEY" und "backendUrl" auf neue Werte. Passe die "messages.json" Datei an und füge neue Nachrichten und Übersetzungen hinzu.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   isProduction: true,
   // name of settings file in google appdatafolder
-  settingsFilename: 'nightrep-settings',
+  settingsFilename: 'order66-settings',
   // api key for dropbox
-  DROPBOX_APP_KEY: 'z5opkgt8251mhsb',
+  DROPBOX_APP_KEY: 'order66',
   // url for backend
   backendUrl: 'https://order66.zreptil.de/backend/index.php',
 };
